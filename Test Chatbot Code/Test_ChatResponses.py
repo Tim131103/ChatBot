69a563066b2fb42299613795c2fd065abf50d050
Lykka2 <lykka.brueggen@bonprix.net>
2025-01-27 14:16:31 +0100

    snake case

--- Test Chatbot Code/Test_ChatResponses.py
+++ Test Chatbot Code/Test_ChatResponses.py
@@ -1,4 +1,7 @@
 import unittest
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '../src'))
 from ChatResponses import ChatResponses
 
 class TestChatResponses(unittest.TestCase):
