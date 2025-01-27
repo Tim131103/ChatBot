0c078ca72619889276b1c841d6036f9d5e1ce8ff
Lykka2 <lykka.brueggen@bonprix.net>
2025-01-27 15:27:14 +0100

    rename

--- src/chat_response.py
+++ src/chat_response.py
@@ -1,10 +1,23 @@
-class ChatResponses:
-    def find_chat_response(intent):
-        intent = "greeting" #Bsp. greeting ausprobieren
+import random
 
-        intent_mapping = {
-        "greeting": "Hallo!"
+class chat_responses:
+    def __init__(self):
+        self.intent_mapping = {
+            "greeting": ["Hallo!", "Guten Tag!"],
+            "goodbye": ["Tschüß!", "Auf Wiedersehen!"],
+            "unknown": ["Entschuldigung, ich habe das nicht verstanden."]
         }
+    
+    
+    
+    def find_chat_response(self, intent):
+        if intent in self.intent_mapping:
+            response = random.choice(self.intent_mapping[intent])
+            return response
+        else :
+            raise ValueError(f"Unknown intent: {intent}")
+        
 
-        response = intent_mapping[intent]
-        return response
+chat_response = chat_responses()
+
+response = chat_response.find_chat_response("greeting")
